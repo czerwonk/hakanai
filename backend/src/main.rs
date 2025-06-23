3008c3adf4e34120040b0c85220469427081dbfc
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-23 17:39:25 +0200

    implement first dummy request

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,1 +1,15 @@
-fn main() {}
+use std::io::Result;
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, web};
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    HttpServer::new(|| App::new().route("/healthz", web::get().to(healthz)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
+async fn healthz(_: String) -> impl Responder {
+    HttpResponse::Ok().body("healthy")
+}
