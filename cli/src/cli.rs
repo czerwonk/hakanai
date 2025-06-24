97b1f3799b60087d6bdf8b81000b1a6aedf1d541
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-06-24 16:10:46 +0200

    define subcommands

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -1,6 +1,7 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use url::Url;
 
-/// Represents the command-line arguments for the server.
+/// Represents the command-line arguments for the application.
 #[derive(Parser)]
 #[command(
     version,
@@ -8,4 +9,25 @@ use clap::Parser;
     author = "Daniel Brendgen-Czerwonk",
     about = "A minimalist one-time secret sharing web service. Share sensitive data through ephemeral links that self-destruct after a single view. No accounts, no tracking, just a simple way to transmit secrets that vanish like morning mist."
 )]
-pub struct Args {}
+pub struct Args {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    /// Receives an ephemeral secret from the server.
+    Get { link: Url },
+
+    /// Send a secret to the server.
+    /// Content is read from stdin.
+    Send {
+        #[arg(
+            short,
+            long,
+            default_value = "http://localhost:8080",
+            help = "Hakanai Server URL to send the secret to (eg. https://hakanai.routing.rocks)."
+        )]
+        server: Url,
+    },
+}
