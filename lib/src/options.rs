e3b9b202294f53aa3afcd1ee407c5551ab414af0
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-07-06 17:08:41 +0200

    doc: add comments for new options

--- lib/src/options.rs
+++ lib/src/options.rs
@@ -3,10 +3,16 @@ use std::time::Duration;
 
 use crate::observer::DataTransferObserver;
 
+/// Options for sending a secret.
 #[derive(Default, Clone)]
 pub struct SecretSendOptions {
+    /// An optional observer to monitor the data transfer.
     pub observer: Option<Arc<dyn DataTransferObserver>>,
+
+    /// An optional chunk size for sending the secret.
     pub chunk_size: Option<usize>,
+
+    /// An optional timeout for sending the secret.
     pub timeout: Option<Duration>,
 }
 
@@ -15,22 +21,26 @@ impl SecretSendOptions {
         Self::default()
     }
 
+    /// Allows setting an observer to monitor the data transfer.
     pub fn with_observer(mut self, observer: Arc<dyn DataTransferObserver>) -> Self {
         self.observer = Some(observer);
         self
     }
 
+    /// Allows setting a custom chunk size for sending a secret.
     pub fn with_chunk_size(mut self, size: usize) -> Self {
         self.chunk_size = Some(size);
         self
     }
 
+    /// Allows setting a custom timeout for sending a secret.
     pub fn with_timeout(mut self, timeout: Duration) -> Self {
         self.timeout = Some(timeout);
         self
     }
 }
 
+/// Options for receiving a secret.
 #[derive(Default, Clone)]
 pub struct SecretReceiveOptions {
     pub chunk_size: Option<usize>,
@@ -42,6 +52,7 @@ impl SecretReceiveOptions {
         Self::default()
     }
 
+    /// Allows setting a custom timeout for receiving a secret.
     pub fn with_timeout(mut self, timeout: Duration) -> Self {
         self.timeout = Some(timeout);
         self
