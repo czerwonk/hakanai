716db809794d143c05c09907fb8d8d70220ddc2c
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-07-12 18:56:16 +0200

    fix: ci build was broken due to nodejs type usage

--- server/src/typescript/common-utils.ts
+++ server/src/typescript/common-utils.ts
@@ -93,7 +93,7 @@ export function debounce<T extends (...args: any[]) => void>(
   func: T,
   wait: number,
 ): (...args: Parameters<T>) => void {
-  let timeout: NodeJS.Timeout | null = null;
+  let timeout: ReturnType<typeof setTimeout> | null = null;
 
   return function executedFunction(...args: Parameters<T>): void {
     if (timeout) clearTimeout(timeout);
