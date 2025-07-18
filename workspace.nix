861cb2dabe8def7e4d2e4e8c47ab3d50a4e959d4
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-07-18 20:41:45 +0200

    fix width of landing page

--- workspace.nix
+++ workspace.nix
@@ -1,4 +1,4 @@
 {
-  version = "2.0.0";
-  cargoHash = "sha256-asxkxcnkkCQ02xVA5pDNrQIb4Yk6xhGRp+XUmz9optg=";
+  version = "2.0.1";
+  cargoHash = "sha256-8gNzAmMQeBI2EiZpP1oY3AtAG477UUX6v1NkxIw4AnQ=";
 }
