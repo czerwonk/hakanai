7bb077ece7206c8f461c0ae45726b8d85f1bcd0c
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-07-19 18:14:01 +0200

    add border on main use cases in homepage

--- workspace.nix
+++ workspace.nix
@@ -1,4 +1,4 @@
 {
-  version = "2.1.3";
-  cargoHash = "sha256-qgNnyNizJlg1OaU8hUHm+h+5wg0RxTwSo5XQL1J7my8=";
+  version = "2.1.4";
+  cargoHash = "sha256-JjI8o7jlb6I0f4fBwIuyRcf4vgfNmKtGea0I0VmDJxc=";
 }
