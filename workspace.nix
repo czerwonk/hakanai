67e811bd234c54e4f1eb27a0a955961653e2e420
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-07-20 17:31:13 +0200

    feat: use banner as background for header, ensure header hight for banner

--- workspace.nix
+++ workspace.nix
@@ -1,4 +1,4 @@
 {
-  version = "2.2.0";
-  cargoHash = "sha256-79/YAqumi53NLcgW08LRlFpNT3j26M2VkVRNs4WKNdE=";
+  version = "2.2.1";
+  cargoHash = "sha256-1PiA5sCdbFVk211uS/+Xyv0Zr/f9sU6f9N6F9uabs3g=";
 }
