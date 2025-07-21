f5fa0e42a51868f09e67e7d0ba92defa15b66dfa
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-07-21 18:15:31 +0200

    fix: try to fix ios shortcuts

--- server/src/web_static.rs
+++ server/src/web_static.rs
@@ -18,7 +18,8 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
         .route("/get-secret.js", web::get().to(serve_get_secret_js))
         .route("/hakanai-client.js", web::get().to(serve_js_client))
         .route("/i18n.js", web::get().to(serve_i18n_js))
-        .route("/ios-shortcuts", web::get().to(serve_ios_shortcuts_js))
+        .route("/ios-shortcuts", web::get().to(serve_ios_shortcuts_html))
+        .route("/ios-shortcuts.js", web::get().to(serve_ios_shortcuts_js))
         .route("/banner.svg", web::get().to(serve_banner))
         .route("/icon.svg", web::get().to(serve_icon))
         .route("/logo.svg", web::get().to(serve_logo))
@@ -237,6 +238,14 @@ async fn serve_privacy(app_data: web::Data<crate::app_data::AppData>) -> impl Re
             .body(html.clone()),
         None => HttpResponse::NotFound().body("No privacy policy configured"),
     }
+}
+
+async fn serve_ios_shortcuts_html() -> impl Responder {
+    serve_with_caching_header(
+        include_bytes!("includes/ios-shortcuts.html"),
+        "text/html",
+        DEFAULT_CACHE_MAX_AGE,
+    )
 }
 
 async fn serve_config(app_data: web::Data<crate::app_data::AppData>) -> impl Responder {
--- workspace.nix
+++ workspace.nix
@@ -1,4 +1,4 @@
 {
-  version = "2.3.2";
-  cargoHash = "sha256-ydF+FKd1mHWcQUUDcYBl7HZmtIpp98jWZHIx2fFs06c=";
+  version = "2.3.3";
+  cargoHash = "sha256-6mWw7jlRlQh6E9j9lRibaOOdQXLZrX/M2S2y65JmPy0=";
 }
