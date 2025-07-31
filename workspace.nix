1a98296bd92c3f0313749a649b3890b23e37bab1
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-07-31 18:37:43 +0200

    chore: update version

--- workspace.nix
+++ workspace.nix
@@ -1,4 +1,4 @@
 {
-  version = "2.6.1";
-  cargoHash = "sha256-TzyyXnUs9rwwChpB4VYwfY06/RAkOIVLPkAmeWqnXsE=";
+  version = "2.6.2";
+  cargoHash = "sha256-3ORzDaBHfuCcj1zLiqI2PyQvF6ql3PpC4F4m4N+0OEk=";
 }
