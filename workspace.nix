1486ad136bb95c017708ab037567253f1bb45262
Daniel Brendgen-Czerwonk <daniel@routing.rocks>
2025-08-17 16:44:22 +0200

    chore: bump version

--- workspace.nix
+++ workspace.nix
@@ -1,4 +1,4 @@
 {
-  version = "2.9.3";
-  cargoHash = "sha256-NH1DTHkRk9MGbXynckP6uQta3zrqBVhFhtDhkyYoFoM=";
+  version = "2.9.4";
+  cargoHash = "sha256-lLsu/dHG0XzKz5+cR8lhnbMrzH2WnQiOPjMpwPLmChQ=";
 }
